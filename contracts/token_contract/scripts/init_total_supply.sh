9eebe1b43dd15d20f5037aa770ec8caa5e8b2289
Nolan <dongnguyenvie@gmail.com>
2022-05-30 15:35:02 +0700

    feat token: Update token smart contract

--- contracts/token_contract/scripts/init_total_supply.sh
+++ contracts/token_contract/scripts/init_total_supply.sh
@@ -5,4 +5,4 @@ CONTRACT_NAME=$(cat ./neardev/dev-account )
 
 # near call $DEV_ACCOUNT new_default_meta '{"owner_id": "'$CONTRACT_NAME'", "total_supply": "1000000000000" }' --accountId $DEV_ACCOUNT
 
-near call $CONTRACT_NAME new '{"owner_id": "'$OWNER_ID'", "total_supply": "1000000000000", "metadata": { "spec": "ft-1.0.0", "name": "Nolan Token", "symbol": "NTK", "decimals": 12 }}' --accountId $CONTRACT_NAME
+near call $CONTRACT_NAME new '{"owner_id": "'$OWNER_ID'", "total_supply": "1000000000000000000000000000", "metadata": { "spec": "ft-1.0.0", "name": "Nolan Token", "symbol": "NTK", "decimals": 18 }}' --accountId $CONTRACT_NAME
